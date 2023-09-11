e683e30392255ccdea816516aa51f1c0e8306886
kamzhanyue <kamzhanyue@gmail.com>
2023-09-11 23:53:37 +0900

    Start Wave System

---
+++ Assets/SleepWalker/Scripts/GameManagement/Wave.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Wave : MonoBehaviour
+{
+    
+    [BoxGroup("Setup Variables")] public UnityEvent onComplete;
+}
