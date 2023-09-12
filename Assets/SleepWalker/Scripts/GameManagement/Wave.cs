1d9a53897566b77402056c7ff526bc0b774a27a9
kamzhanyue <kamzhanyue@gmail.com>
2023-09-12 21:39:25 +0900

    Setup Boss Battle Objects

--- Assets/SleepWalker/Scripts/GameManagement/Wave.cs
+++ Assets/SleepWalker/Scripts/GameManagement/Wave.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
@@ -6,6 +7,68 @@ using UnityEngine.Events;
 
 public class Wave : MonoBehaviour
 {
-    
+    [BoxGroup("Setup Variables")] public GameObjectRuntimeSet enemyRuntimeSet;
+    [BoxGroup("Setup Variables")] public GameObject enemyLightPrefab;
+    [BoxGroup("Setup Variables")] public GameObject enemyHeavyPrefab;
+    [BoxGroup("Setup Variables")] public List<WaveObject> enemies;
     [BoxGroup("Setup Variables")] public UnityEvent onComplete;
+
+    private bool activated = false;
+    
+    [Serializable]
+    public class WaveObject
+    {
+        public Transform transform;
+        public EnemyType enemyType;
+    }
+    
+    [Serializable]
+    public enum EnemyType
+    {
+        Light = 0,
+        Heavy = 1
+    }
+
+    [Button]
+    public void Activate()
+    {
+        activated = true;
+        for (int i = 0; i < enemies.Count; ++i)
+        {
+            Spawn(enemies[i]);
+        }
+    }
+
+
+    private void Spawn(WaveObject _waveObject)
+    {
+        GameObject enemy;
+        switch (_waveObject.enemyType)
+        {
+            case EnemyType.Light: 
+                enemy = Instantiate(enemyLightPrefab, _waveObject.transform);
+                enemy.SetActiveFast(true);
+                break;
+            case EnemyType.Heavy:
+                enemy = Instantiate(enemyHeavyPrefab, _waveObject.transform);
+                enemy.SetActiveFast(true);
+                break;
+        }
+    }
+
+    public void OnEnemyRemoved(GameObject _enemy)
+    {
+        if (!activated)
+            return;
+        if (enemyRuntimeSet.items.Count <= 0)
+        {
+            OnComplete();
+            activated = false;
+        }
+    }
+
+    private void OnComplete()
+    {
+        onComplete?.Invoke();
+    }
 }
--- Assets/SleepWalker/Scripts/GameManagement/WaveController.cs
+++ Assets/SleepWalker/Scripts/GameManagement/WaveController.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class WaveController : MonoBehaviour
 {
 
+    public void Spawn(Wave.WaveObject _waveObject)
+    {
+        switch (_waveObject.enemyType)
+        {
+            case Wave.EnemyType.Light:
+                
+                break;
+            case Wave.EnemyType.Heavy:
+                break;
+        }
+    }
 }
