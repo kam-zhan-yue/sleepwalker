029720b391894e518c69c25e7decb42fda202e3d
kamzhanyue <kamzhanyue@gmail.com>
2023-09-18 13:36:34 +0900

    Implement Button Animations

--- Assets/SleepWalker/Scripts/UI/MainMenuPopup.cs
+++ Assets/SleepWalker/Scripts/UI/MainMenuPopup.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using MEC;
+using Sirenix.OdinInspector;
 using UnityAtoms.BaseAtoms;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuPopup : Popup
 {
-    public LevelSelectPopup levelSelectPopup;
-    public SettingsPopup settingsPopup;
-    public CreditsPopup creditsPopup;
-    public BoolVariable showLevelSelection;
+    [BoxGroup("UI Objects")] public LevelSelectPopup levelSelectPopup;
+    [BoxGroup("UI Objects")] public SettingsPopup settingsPopup;
+    [BoxGroup("UI Objects")] public CreditsPopup creditsPopup;
+    [BoxGroup("UI Objects")] public BoolVariable showLevelSelection;
+    [BoxGroup("Animation")] public List<GameObject> buttonList = new();
+    [BoxGroup("Animation")] public float animationDuration = 1f;
+    [BoxGroup("Animation")] public float animationDelay = 1f;
+    [BoxGroup("Animation")] public Ease easing = Ease.OutElastic;
+
+    private CoroutineHandle buttonRoutine;
     
     protected override void InitPopup()
     {
@@ -39,6 +48,36 @@ public class MainMenuPopup : Popup
             levelSelectPopup.HidePopup();
             settingsPopup.HidePopup();
             creditsPopup.HidePopup();
+            PlayButtonAnimations();
+        }
+    }
+
+    [Button]
+    private void PlayButtonAnimations()
+    {
+        // Sequence sequence = DOTween.Sequence();
+        // for (int i = 0; i < buttonList.Count; ++i)
+        // {
+        //     buttonList[i].gameObject.transform.localScale = Vector3.zero;
+        //     sequence.Append(buttonList[i].gameObject.transform.DOScale(Vector3.one, animationDuration).SetEase(easing));
+        //     sequence.AppendInterval(animationDelay);
+        // }
+        // sequence.Play();
+        // Debug.Log("Playing Sequence");
+        buttonRoutine = Timing.RunCoroutine(ButtonAnimations().CancelWith(gameObject));
+    }
+
+    private IEnumerator<float> ButtonAnimations()
+    {
+        for (int i = 0; i < buttonList.Count; ++i)
+        {
+            buttonList[i].gameObject.transform.localScale = Vector3.zero;
+        }
+        
+        for (int i = 0; i < buttonList.Count; ++i)
+        {
+            buttonList[i].gameObject.transform.DOScale(Vector3.one, animationDuration).SetEase(easing);
+            yield return Timing.WaitForSeconds(animationDelay);
         }
     }
 
